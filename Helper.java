8317cb82ebf0309b3de22119bfed9d9063bb5750
bhakti27 <bhaktis327@gmail.com>
2022-07-22 10:59:46 -0700

    add getters

--- Helper.java
+++ Helper.java
@@ -1,5 +1,16 @@
 public class Helper {
 
+    public int getCount() {
+        return count;
+    }
+
+    public int getSats() {
+        return sats;
+    }
+
     private int count;
     private int sats;
+
+
+
 }
